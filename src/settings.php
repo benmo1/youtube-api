2d139d399cd03934ea3098199e28c648d950fd60
Ben Morris <benjamin.morris77@gmail.com>
2019-05-31 19:45:36 +0100

    Set test environment in phpunit
    This stops errors from being swallowed in the
    response object in a test environment

--- src/settings.php
+++ src/settings.php
@@ -2,7 +2,7 @@
 
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
+        'displayErrorDetails' => $_ENV['APPLICATION_ENV'] === 'development', // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
         // Monolog settings
