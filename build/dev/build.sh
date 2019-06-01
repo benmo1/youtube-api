496d7f8cc9d825d7093183877ca11ef4b4bb5e37
Ben Morris <benjamin.morris77@gmail.com>
2019-06-02 00:33:05 +0100

    Uncomment debug lines in build.sh

--- build/dev/build.sh
+++ build/dev/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 echo "Installing vendor libraries..."
-#if [ "$(docker images -q local:php 2> /dev/null)" == "" ]; then
+if [ "$(docker images -q local:php 2> /dev/null)" == "" ]; then
     docker build ./slim/ -t local:php
-#fi
-#if [ "$(docker images -q mycomposer 2> /dev/null)" == "" ]; then
+fi
+if [ "$(docker images -q mycomposer 2> /dev/null)" == "" ]; then
     docker build ./composer -t mycomposer
-#fi
+fi
 docker run --rm --interactive --tty --volume "$PWD/../../":/app -w="/app" mycomposer install --no-plugins --no-scripts
 
 if ! [ -f "../../.env" ] ; then
