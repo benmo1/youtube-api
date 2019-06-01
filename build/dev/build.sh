f3c8bdf40ef0ecb9bb30e28b8cb7c3cc338b08a6
Ben Morris <benjamin.morris77@gmail.com>
2019-06-01 23:51:20 +0100

    Make composer Dockerfile more flexible
    This means that you can use other composer commands than install.
    However, you need to take care to always use --no-plugins --no-scripts
    as composor runs as the root user so is insecure.

--- build/dev/build.sh
+++ build/dev/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 echo "Installing vendor libraries..."
-if [ "$(docker images -q local:php 2> /dev/null)" == "" ]; then
+#if [ "$(docker images -q local:php 2> /dev/null)" == "" ]; then
     docker build ./slim/ -t local:php
-fi
-if [ "$(docker images -q mycomposer 2> /dev/null)" == "" ]; then
+#fi
+#if [ "$(docker images -q mycomposer 2> /dev/null)" == "" ]; then
     docker build ./composer -t mycomposer
-fi
-docker run --rm --interactive --tty --volume "$PWD/../../":/app -w="/app" mycomposer
+#fi
+docker run --rm --interactive --tty --volume "$PWD/../../":/app -w="/app" mycomposer install --no-plugins --no-scripts
 
 if ! [ -f "../../.env" ] ; then
     echo "Creating .env file..."
