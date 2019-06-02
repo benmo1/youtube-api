ebb00089154bf0e4aa30085ee00dcb3cab085e03
Ben Morris <benjamin.morris77@gmail.com>
2019-06-02 17:13:52 +0100

    Pull test fixtures into separate directory
    This gives clearer visitbility.

---
+++ tests/Functional/fixtures/videos.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'videos' => [
+        [
+            'title' => 'Bikes in the 21st Century',
+            'date' => '2019-04-23',
+        ],
+        [
+            'title' => 'Master cyclist on tour',
+            'date' => '2018-04-29',
+        ],
+        [
+            'title' => 'What goes around comes around, a look at alimunium wheels',
+            'date' => '2019-03-23',
+        ],
+    ],
+];
