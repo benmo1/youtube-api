d060e2608fbbd8d1bd2e43ce521aebf4a3f5354c
Ben Morris <benjamin.morris77@gmail.com>
2019-06-02 01:02:57 +0100

    Remove unused test

--- tests/Functional/QueryTest.php
+++ tests/Functional/QueryTest.php
@@ -70,29 +70,6 @@ class QueryTest extends BaseTestCase
         $this->assertEquals('{"error":"Invalid search characters - must be numeric, alpha, or single spaces."}', (string) $response->getBody());
     }
 
-    public function videoData()
-    {
-        $this->markTestSkipped('For TDD');
-
-        return [
-            [
-                'id' => 1,
-                'title' => 'Bikes in the 21st Century',
-                'date' => '2019-04-23',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Master cyclist on tour',
-                'date' => '2018-04-29',
-            ],
-            [
-                'id' => 3,
-                'title' => 'What goes around comes around, a look at alimunium wheels',
-                'date' => '2019-03-23',
-            ],
-        ];
-    }
-
     public function badCharacters()
     {
         return array_map(function ($ch) {
